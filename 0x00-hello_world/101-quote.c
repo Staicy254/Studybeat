b82d351a2bb95d0c366b2552b46fb7883c353d70
Staicy254 <jeanstaicy21@gmail.com>
2023-06-29 15:30:38 -0700

    Unix system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 1 (success)
+ */
+
+int main(void)
+{
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
+
