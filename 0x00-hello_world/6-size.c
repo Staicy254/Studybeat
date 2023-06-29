3ee9674bd86734139459f9a02337c60d36bee81e
Staicy254 <jeanstaicy21@gmail.com>
2023-06-29 15:15:39 -0700

    Intel Syntax

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", size of (char));
+printf("Size of an int: %d byte(s)\n", size of (int));
+printf("Size of a long int: %d byte(s)\n", size of (long int));
+printf("Size of a long long int: %d byte(s)\n", size of (long long int));
+printf("Size of a float: %d byte(s)\n", size of (float));
+
+return (0);
+}
